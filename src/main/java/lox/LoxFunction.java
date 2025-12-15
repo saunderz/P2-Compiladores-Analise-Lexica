f5ba1972ec9e29397015a0f1211a9264583ba6c1
Melissa Palhano <mlssplhn@gmail.com>
2025-12-15 18:25:20 -0300

    feat: resolve methods, this and initializers

--- src/main/java/lox/LoxFunction.java
+++ src/main/java/lox/LoxFunction.java
@@ -5,8 +5,11 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
   private final Environment closure;
+  private final boolean isInitializer;
 
-  LoxFunction(Stmt.Function declaration, Environment closure) {
+  LoxFunction(Stmt.Function declaration, Environment closure,
+              boolean isInitializer) {
+    this.isInitializer = isInitializer;
     this.closure = closure;
     this.declaration = declaration;
   }
@@ -23,14 +26,22 @@ class LoxFunction implements LoxCallable {
     try {
       interpreter.executeBlock(declaration.body, environment);
     } catch (Return returnValue) {
+      if (isInitializer) return closure.getAt(0, "this");
       return returnValue.value;
     }
+    if (isInitializer) return closure.getAt(0, "this");
     return null;
   }
 
   @Override
   public int arity() {
     return declaration.params.size();
+  }
+
+  LoxFunction bind(LoxInstance instance) {
+    Environment environment = new Environment(closure);
+    environment.define("this", instance);
+    return new LoxFunction(declaration, environment, isInitializer);
   }
 
   @Override
